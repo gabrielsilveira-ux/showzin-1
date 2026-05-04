943ef5799682a8882ac239c257a9304fa2572edd
Gabriel Silveira <gabrielsilveira.mkt@gmail.com>
2026-05-04 01:24:40 -0300

    fix: remove google font fetch dependency from build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/components/core/ReduxProvider";
 import ApplicationWrapper from "@/components/core/ApplicationWrapper";
 import QueryProvider from "@/components/core/QueryProvider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Showzin Admin",
@@ -29,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="antialiased"
       >
         <ReduxProvider>
           <ApplicationWrapper>
